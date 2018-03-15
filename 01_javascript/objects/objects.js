716728c70ed9c0f4d52ef6c98f305695d5e551da
Michael Hebblethwaite <mhhebble@gmail.com>
2018-03-15 15:47:47 +1100

    add inro to objects

---
+++ 01_javascript/objects/objects.js
@@ -0,0 +1,30 @@
+console.log('objects')
+
+/*
+
+say you want to store your favourite recipe.
+it should have a title, servings and ingredients
+
+output
+
+var pancakes
+servings: 2
+ingredients: 
+flour
+egg
+milk
+butter
+sugar
+
+
+
+
+*/
+
+var recipe = {
+  title: 'pancakes',
+  servings: 4,
+  ingredients: ['flour', 'egg', 'milk', 'butter', 'sugar']
+}
+
+var people = [{ name: 'dt', age = 400},{ name: 'Luke', age: 50 }]
