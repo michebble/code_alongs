4ec21cb0128c6f0d1dc81c8bba3c348779da788c
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-03 15:42:52 +1000

    change comparison from string to integer

--- 04_ruby/ruby_intro/guessing_game.rb
+++ 04_ruby/ruby_intro/guessing_game.rb
@@ -4,13 +4,13 @@ require 'pry'
 puts 'Welcome to the guessing game.'
 puts 'Please guess a number between 1 and 10.'
 guess = gets.chomp   
-secret_number = rand(10) + 1
-
-while guess != secret_number.to_s
-  if guess > secret_number.to_s
+secret_number = rand(1..10) 
+binding.pry
+while guess.to_i != secret_number
+  if guess.to_i > secret_number
     puts 'The secret number is less than that.'
     guess = gets.chomp 
-  elsif guess < secret_number.to_s
+  elsif guess.to_i < secret_number
     puts 'The secret number is higher than that.'
     guess = gets.chomp 
   end
