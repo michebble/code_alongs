5f50ecf02a9ca5a8a9be7fb1636fd43eb530bd25
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-03 15:00:04 +1000

    introduce interpolation

--- 04_ruby/ruby_intro/intro.rb
+++ 04_ruby/ruby_intro/intro.rb
@@ -8,3 +8,21 @@ movie = {
   title: 'The Room',
   score: 8
 }
+
+# string .to_s
+# integer .to_i
+# float .to_f
+# boolean TrueClass & FalseClass
+# array
+# NilClass
+# symbol
+# hash
+
+first_name = 'Jack'
+last_name = 'Black'
+puts(first_name + '  ' + last_name)
+
+puts("hello #{first_name} #{last_name}")
+
+print first_name
+print first_name
