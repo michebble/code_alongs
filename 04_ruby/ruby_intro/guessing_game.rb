c1e1e312f1319709984c2c88a4970cc95598978e
Michael Hebblethwaite <mhhebble@gmail.com>
2018-04-03 15:35:26 +1000

    improve incorrect guess feedback

--- 04_ruby/ruby_intro/guessing_game.rb
+++ 04_ruby/ruby_intro/guessing_game.rb
@@ -6,9 +6,14 @@ puts 'Please guess a number between 1 and 10.'
 guess = gets.chomp   
 secret_number = rand(10) + 1
 
-while guess != secret.to_s
-  puts 'Incorrect. Guess again.'
-  guess = gets.chomp 
+while guess != secret_number.to_s
+  if guess > secret_number.to_s
+    puts 'The secret number is less than that.'
+    guess = gets.chomp 
+  elsif guess < secret_number.to_s
+    puts 'The secret number is higher than that.'
+    guess = gets.chomp 
+  end
 end
 
 puts("You win, #{secret_number} was the correct number.")
